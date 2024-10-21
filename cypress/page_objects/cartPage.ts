d6efe4f8a159d6bf0fae674b4009d829ca2f9b61
mib-coding <masroor.iqbal.buttar@gmail.com>
2024-10-21 21:17:19 +0200

    cart item locators

--- cypress/page_objects/cartPage.ts
+++ cypress/page_objects/cartPage.ts
@@ -1,7 +1,11 @@
 //this page locates cart items and cart page
 class CartPage {
     //cart locators
-
+    CartItem = {
+        listing: () => cy.xpath(''),
+        listingButton: () => cy.xpath(''),
+        listingMenu: () => cy.xpath(''),
+        }
 }
   export default new CartPage();
   
